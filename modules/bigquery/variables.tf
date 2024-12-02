76886f049abe3f208f311421fd97dae7a5cd11b1
sumanthdevopsgit <93962528+sumanthdevopsgit@users.noreply.github.com>
2024-12-02 14:38:03 +0530

    Add files via upload

---
+++ modules/bigquery/variables.tf
@@ -0,0 +1,18 @@
+variable "project_id" {
+  description = "Google Cloud project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  type        = string
+}
+variable "dataset_id" {
+  description = "Dataset ID"
+  type        = string
+}
+
+variable "table_id" {
+  description = "Table ID"
+  type        = string
+}
