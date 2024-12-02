64f049a8a8decf76f32cca19f8d5a4f8523577c2
sumanthdevopsgit <93962528+sumanthdevopsgit@users.noreply.github.com>
2024-12-02 14:42:12 +0530

    Add files via upload

---
+++ modules/pub_sub/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  description = "Google Cloud project ID"
+  type        = string
+}
+variable "topic_name" {
+  description = "Google Cloud Pub/Sub Topic name"
+  type        = string
+}
