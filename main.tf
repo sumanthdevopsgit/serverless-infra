44a5361ec6a86857b4f67555e84e001a933bc037
sumanthdevopsgit <93962528+sumanthdevopsgit@users.noreply.github.com>
2024-12-02 14:35:51 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,54 +1,40 @@
-module "database_migration" {
-  source            = "./modules/database_migration"
-  environment       = var.environment
-  project_id        = var.project_id
-  source_username   = var.source_username
-  source_password   = var.source_password
-  source_host       = var.source_host
-  region            = var.region
-  sql_user_name     = var.sql_user_name
-  sql_user_password = var.sql_user_password
-}
-module "bigquery" {
-  source     = "./modules/bigquery"
-  project_id = var.project_id
-  region     = var.region
-  dataset_id = var.dataset_id
-  table_id   = var.table_id
+resource "google_bigquery_dataset" "autozone_dataset" {
+  project    = var.project_id
+  dataset_id = var.dataset_id   #"autozone_dataset_poc2"
+  location   = var.region
+  
 }
 
-module "pubsub" {
-  source     = "./modules/pubsub"
-  project_id = var.project_id
-  topic_name = var.topic_name
-}
-
-module "scheduler" {
-  source              = "./modules/scheduler"
-  project_id          = var.project_id
-  topic_name          = module.pubsub.autozone_topic_name
-  scheduler_frequency = var.scheduler_frequency
-  job_name            = var.job_name #"autozone-scheduler-job"
-  time_zone           = var.time_zone
-}
-# To start the function automatically.
-# resource "null_resource" "trigger_scheduler_job" {
-#   provisioner "local-exec" {
-#     command = "gcloud scheduler jobs run ${module.scheduler.job_name} --location=${var.region}"
-#   }
-
-#   # Ensure this runs after the scheduler job is created
-#   depends_on = [module.scheduler]
-# }
-
-module "cloud_function" {
-  source                = "./modules/cloud_function"
-  project_id            = var.project_id
-  region                = var.region
-  service_account_email = var.service_account_email
-  bucket_name           = var.bucket_name
-  source_path           = var.source_path
-  topic_name            = module.pubsub.autozone_topic_name
-  function_name         = var.function_name
-  force_destroy         = var.force_destroy
+resource "google_bigquery_table" "autozone_table" {
+  project    = var.project_id
+  dataset_id = google_bigquery_dataset.autozone_dataset.dataset_id
+  table_id   = var.table_id #"poc2_Table"
+  deletion_protection = false
+  schema     = jsonencode([
+    {
+      name = "Table_Name"
+      type = "STRING"
+      mode = "REQUIRED"
+    },
+    {
+      name = "Source_Count"
+      type = "INTEGER"
+      mode = "REQUIRED"
+    },
+    {
+      name = "Destination_Count"
+      type = "INTEGER"
+      mode = "REQUIRED"
+    },
+    {
+      name = "Status"
+      type = "STRING"
+      mode = "REQUIRED"
+    },
+    {
+      name = "Validation_Date"
+      type = "TIMESTAMP"
+      mode = "REQUIRED"
+    }
+  ])
 }
--- variables.tf
+++ variables.tf
@@ -1,71 +1,12 @@
-variable "region" {
-  description = "Region for the database migration."
-  type        = string
-}
-
-variable "environment" {
-  description = "The environment (dev or prod)."
-  type        = string
-}
-
-variable "source_host" {
-  description = "Host for the source MySQL database."
-  type        = string
-}
-
-variable "source_username" {
-  description = "Username for the source MySQL database."
-  type        = string
-}
-
-variable "source_password" {
-  description = "Password for the source MySQL database."
-  type        = string
-  sensitive   = true
-}
-
 variable "project_id" {
-  description = "The GCP project ID."
+  description = "Google Cloud project ID"
   type        = string
 }
 
-variable "sql_user_name" {
-  description = "The name of the SQL user to be created in Cloud SQL."
+variable "region" {
+  description = "Google Cloud region"
   type        = string
 }
-
-variable "sql_user_password" {
-  description = "The password for the SQL user to be created in Cloud SQL."
-  type        = string
-  sensitive   = true
-}
-
-variable "scheduler_frequency" {
-  description = "Cron schedule for Cloud Scheduler"
-  type        = string
-}
-
-variable "service_account_email" {
-  description = "Service account email for Cloud Function"
-  type        = string
-}
-
-variable "bucket_name" {
-  description = "GCS bucket name for Cloud Function source code"
-  type        = string
-}
-
-variable "source_path" {
-  description = "Path to the Cloud Function source code zip file"
-  type        = string
-}
-
-variable "job_name" {
-  description = "Name of the Cloud Scheduler job"
-  type        = string
-  default     = "autozone-scheduler-job"
-}
-
 variable "dataset_id" {
   description = "Dataset ID"
   type        = string
@@ -73,25 +14,5 @@ variable "dataset_id" {
 
 variable "table_id" {
   description = "Table ID"
-  type        = string
-}
-
-variable "time_zone" {
-  description = "Time zone for the Cloud Scheduler job"
-  type        = string
-}
-
-variable "topic_name" {
-  description = "Google Cloud Pub/Sub Topic name"
-  type        = string
-}
-
-variable "function_name" {
-  description = "Cloud Function name"
-  type        = string
-}
-
-variable "force_destroy" {
-  description = "Storage bucket force destroy"
   type        = string
 }
